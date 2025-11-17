46455f7c51a4be7b84c085012379e50ce9f149ab
santiagofalcon <santiagofalcon12@hotmail.com>
2025-11-17 15:34:23 +0100

    Initial commit

---
+++ rickAndMortySwiftUI/rickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortySwiftUIApp.swift
+//  rickAndMortySwiftUI
+//
+//  Created by Santiago Falcon Gonzalez on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
